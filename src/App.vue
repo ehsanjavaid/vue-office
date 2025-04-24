fdd4b4f4408b678b2520a1e5e5af04de96f8a72b
Ahsan Javaid <124583633+ehsanjavaid@users.noreply.github.com>
2025-04-24 11:58:28 +0500

    confetti config and test

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
-<Badge
-@click="showConfetti"
-  :variant="'subtle'"
-  :ref_for="true"
-  theme="green"
-  size="lg"
-  label="Badge"
->
-  Badge
-</Badge>
+  <Badge @click="showConfetti" :variant="'subtle'" :ref_for="true" theme="green" size="lg" label="Badge">
+    Badge
+  </Badge>
+  <Badge @click="showDialog = true" :variant="'subtle'" :ref_for="true" theme="green" size="lg" label="Badge">
+    Show Dialog
+  </Badge>
+  
+  <Dialog v-model="showDialog" :options="{
+    title: 'Confirm',
+    message: 'Are you sure you want to confirm this action?',
+    size: 'xl',
+  }" />
 </template>
 <script setup>
 import Badge from 'frappe-ui/src/components/Badge.vue';
+import Dialog from 'frappe-ui/src/components/Dialog.vue';
 import confetti from 'canvas-confetti';
+// import { Badge, Dialog, Button } from 'frappe-ui';
+// import { dialogs } from 'frappe-ui/src/utils/confirmDialog';
+import { ref } from 'vue';
+const showDialog = ref(false);
 function showConfetti() {
   confetti({
     particleCount: 200,
